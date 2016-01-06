8d22d269ffeaf38a8d6543bf0ac3a8d76a900b17
Aaron McTavish <aamct@ustwo.com>
2016-01-06 09:43:15 +0000

    Set up Xcode workspace and iOS targets

---
+++ Sources/BaseViewControllerSwift.h
@@ -0,0 +1,19 @@
+//
+//  BaseViewControllerSwift.h
+//  BaseViewControllerSwift
+//
+//  Created by Aaron McTavish on 06/01/2016.
+//  Copyright © 2016 BaseViewControllerSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseViewControllerSwift.
+FOUNDATION_EXPORT double BaseViewControllerSwiftVersionNumber;
+
+//! Project version string for BaseViewControllerSwift.
+FOUNDATION_EXPORT const unsigned char BaseViewControllerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseViewControllerSwift/PublicHeader.h>
+
+
